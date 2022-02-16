e5ede0600b1c9428a84b9bef3d02528bca7292b3
etoolabode <etoolabode@gmail.com>
2022-02-16 13:04:52 +0100

    javascript added

---
+++ js/app.js
@@ -0,0 +1,11 @@
+window.onscroll = function () {
+  myFunction();
+};
+function myFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    console.log("Hello scroll");
+    document.getElementById("heroicons").className = "move-hero-icons";
+  } else {
+    document.getElementById("heroicons").className = "hero-icons";
+  }
+}
